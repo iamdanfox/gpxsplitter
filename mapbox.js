2499e99b7c799c2f85c6cef607568acc1bc47b78
Dan Fox <danf@palantir.com>
2014-08-08 00:36:15 -0700

    Display a line

--- mapbox.js
+++ mapbox.js
@@ -10,19 +10,41 @@ MapBox = React.createClass({
     styleUrl: 'https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v4.json'
   },
   componentDidMount: function() {
-    var m;
+    var geoJSON, map, route;
     mapboxgl.accessToken = this.props.apiToken;
-    m = new mapboxgl.Map({
+    mapboxStyle.layers.push({
+      "id": "route",
+      "source": "route",
+      "render": {
+        "$type": "LineString",
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "style": {
+        "line-color": "#FF4136",
+        "line-width": 3
+      },
+      "type": "line"
+    });
+    map = new mapboxgl.Map({
       container: this.props.id,
       style: mapboxStyle,
-      center: [40, -74.50],
-      zoom: 9
+      center: [37.830348, -122.486052],
+      zoom: 15
     });
-    return console.debug(m);
-  },
-  shouldComponentUpdate: function(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return false;
+    geoJSON = {
+      "type": "Feature",
+      properties: {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[-122.48369693756104, 37.83381888486939], [-122.48348236083984, 37.83317489144141], [-122.48339653015138, 37.83270036637107], [-122.48356819152832, 37.832056363179625], [-122.48404026031496, 37.83114119107971], [-122.48404026031496, 37.83049717427869], [-122.48348236083984, 37.829920943955045], [-122.48356819152832, 37.82954808664175], [-122.48507022857666, 37.82944639795659], [-122.48610019683838, 37.82880236636284], [-122.48695850372314, 37.82931081282506], [-122.48700141906738, 37.83080223556934], [-122.48751640319824, 37.83168351665737], [-122.48803138732912, 37.832158048267786], [-122.48888969421387, 37.83297152392784], [-122.48987674713133, 37.83263257682617], [-122.49043464660643, 37.832937629287755], [-122.49125003814696, 37.832429207817725], [-122.49163627624512, 37.832564787218985], [-122.49223709106445, 37.83337825839438], [-122.49378204345702, 37.83368330777276]]
+      }
+    };
+    route = new mapboxgl.GeoJSONSource({
+      data: geoJSON
+    });
+    map.addSource('route', route);
+    return console.debug(map);
   },
   render: function() {
     return React.DOM.div({
